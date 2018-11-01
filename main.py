371be73848529ce184a79a72833cdf85fcc669df
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-01 15:29:48 -0500

    add if __name__

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Task(db.Model):
     def __init__(self, name):
         self.name = name 
 
-        
+
 
 tasks = []
 
@@ -29,4 +29,5 @@ def index():
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
 
+if __name__ == '__main__':
 app.run()
