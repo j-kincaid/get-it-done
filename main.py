62816d142ebf7d3049ba54e8f3cf71f74cccb689
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-01 15:24:51 -0500

    add SQLAlchemy

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:greenenchiladas@localhost:8889'
+app.config['SQLALCHEMY_ECHO'] True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
 
 tasks = []
 
