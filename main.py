82577408bf241cc10045297cd93f6b0bd8d8c4e1
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-02 14:21:55 -0500

    create columns and class

--- main.py
+++ main.py
@@ -3,18 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:greenenchiladas@localhost:8889'
-app.config['SQLALCHEMY_ECHO'] True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:greenenchiladas@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+
 db = SQLAlchemy(app)
 
 class Task(db.Model):
+# Specify data fields that go into the class, in columns
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
 
+# Add a constructor
     def __init__(self, name):
         self.name = name 
 
+# def email_valid(email):
+#     if not @ in email:
+#         return False
+
+#     if len(email) <= 4:
+#         return False
+
+#     if not ('.' in email):
+#         return False
+
+#     if ' ' in email:
+#         return False
+
+#     return True
 
 
 tasks = []
