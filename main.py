079615c3356445b1fdb47b59fd1e16eea4bd20e6
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-05 14:20:28 -0600

    copied annotated version

--- main.py
+++ main.py
@@ -18,22 +18,38 @@ class Task(db.Model):
 # Every class has a primary key
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
 # Add a constructor
     def __init__(self, name):
         self.name = name 
+        self.completed = False
 
-
-tasks = []
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
         task = request.form['task']
-        tasks.append(task)
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = 
+Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
 
 # Shield the app.run() file
 if __name__ == '__main__':
