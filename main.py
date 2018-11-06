ac0f9e4586fbb3e078d8d6ea186a5bf02b2213d7
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-06 16:12:43 -0600

    added boolean to indicate if tasks had been completed, edited UX

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:greenenchiladas@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-
 db = SQLAlchemy(app)
 
-# class extends the db.Model object
+# (flask-env) $ python
+# from main import db, Blog
+# db.create_all()
+# db.session.commit()
 
-# Specify data fields that go into the class, in columns
-# in Terminal write: tasks = Task.query.all()
+class Task(db.Model): # We added a new column:
 
-class Task(db.Model):
-
-# Every class has a primary key
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    completed = db.Column(db.Boolean, default = False)
 
-# Add a constructor
     def __init__(self, name):
-        self.name = name 
+        self.name = name
         self.completed = False
 
 
@@ -30,27 +27,32 @@ class Task(db.Model):
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        new_task = Task(task_name)
+        task_name = request.form['task']
+        new_task = Task(task_name) # Create a new task object
         db.session.add(new_task)
-        db.session.commit()
+        db.session.commit() # commit it to the db
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = 
-Task.query.filter_by(completed=True).all()
+    tasks = Task.query.filter_by(completed=False).all() 
+    # only give me the tasks for which the completed columnhas the value False
+    completed_tasks = Task.query.filter_by(completed=True).all()
     return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
 
+
 @app.route('/delete-task', methods=['POST'])
+# Take the given task id that's posted through the form and delete it from the db
 def delete_task():
 
     task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
+    task = Task.query.get(task_id) # gets the specific task object by numeric id from the db
+
     task.completed = True
     db.session.add(task)
     db.session.commit()
+    # db.session.delete(task)
+    # db.session.commit()
 
-    return redirect('/')
+    return redirect('/') # redirect back to the main.py
 
-# Shield the app.run() file
+
 if __name__ == '__main__':
     app.run()
